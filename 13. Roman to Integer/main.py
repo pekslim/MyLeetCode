007ca7a8b3ed8741d31e9dfbf664516a9f521be5
peks <prod.pxl@gmail.com>
2022-01-17 02:22:57 -0500

    1/16/2022: P13

---
+++ 13. Roman to Integer/main.py
@@ -0,0 +1,25 @@
+class Solution:
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        x = 0
+
+        for i in s:
+            x += roman_dict[i]
+
+        if 'IV' in s or 'IX' in s:
+            x -= 2
+        if 'XL' in s or 'XC' in s:
+            x -= 20
+        if 'CD' in s or 'CM' in s:
+            x -= 200
+        return x
+
+if __name__ == '__main__':
+    solution = Solution()
+    print(solution.romanToInt("MCMXCIV"))
+
+
