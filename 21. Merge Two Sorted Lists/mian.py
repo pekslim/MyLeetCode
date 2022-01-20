abe1f808cdb5fc4639cf02f27219a56209f39509
peks <prod.pxl@gmail.com>
2022-01-19 19:14:50 -0500

    1/19/2021: P21 (Linked list, fail)

---
+++ 21. Merge Two Sorted Lists/mian.py
@@ -0,0 +1,36 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+from typing import Optional
+
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        curr = ListNode(0)
+        dummy = curr
+
+        while list1 and list2:
+            if list1.val < list2.val:
+                curr.next = list1
+                list1 = list1.next
+            else:
+                curr.next = list2
+                list2 = list2.next
+            curr = curr.next
+        curr.next = list1 or list2
+        return dummy.next
+
+###########################################################################################################
+
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        if list1 == None: return list2
+        if list2 == None: return list1
+
+        if list1.val <= list2.val:
+            list1.next = self.mergeTwoLists(list1.next, list2)
+        else:
+            list2.next = self.mergeTwoLists(list1, list2.next)
+
+        return list1 if list1.val <= list2.val else list2
